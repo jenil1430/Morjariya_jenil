09df367f0d32b3a90608cf2869dbf01489879bce
jenil1430 <jenilmorjarolya@gmail.com>
2025-01-28 02:24:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -156,9 +156,9 @@ FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else 
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'DeendayalBot'))
+SESSION_NAME = str(environ.get('SESSION_NAME', '@Hulkjp_bot'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'Deendayal'))
+name = str(environ.get('name', 'naitik'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
